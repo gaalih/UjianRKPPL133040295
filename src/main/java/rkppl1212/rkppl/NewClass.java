2d6a5f5b9a0a3837be470bc57b25601e8b124521
bocahjowo <bocah.jowo25@gmail.com>
2016-05-13 16:23:46 +0700

    First Commit

---
+++ src/main/java/rkppl1212/rkppl/NewClass.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rkppl1212.rkppl;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import org.apache.log4j.Logger;
+import org.apache.log4j.chainsaw.Main;
+
+/**
+ *
+ * @author SB604-22
+ */
+public class NewClass {
+    static Logger log = Logger.getLogger(Main.class.getName());
+   
+   public static void main(String[] args)throws IOException,SQLException{
+    
+   }
+}
